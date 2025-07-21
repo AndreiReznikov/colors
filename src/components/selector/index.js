572f15b05dd2142aa714a6e3205e9e12679a3873
AndreiReznikov <andrezniksk@yandex.ru>
2025-07-21 17:36:15 +0300

    fix selector logic

--- src/components/selector/index.js
+++ src/components/selector/index.js
@@ -3,8 +3,17 @@ import { toggleScroll } from "~utils/utils";
 import Selector from './Selector';
 
 const initializeSelector = () => {
-  const selector = new Selector();
-  selector.init({ toggleScroll });
+  const selectorElements = document.querySelectorAll('.selector');
+
+  selectorElements.forEach((element) => {
+    const options = {
+      onAction: () => toggleScroll(),
+      onClick: () => toggleScroll(),
+      onClose: () => toggleScroll(),
+    };
+
+    new Selector(element, options);
+  });
 };
 
 document.addEventListener('DOMContentLoaded', initializeSelector);
